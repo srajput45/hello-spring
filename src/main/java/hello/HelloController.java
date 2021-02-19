af47e263d1a09e51761bbd984c9fc0d5da57658e
Shubham Rajput <rajputshubham45@gmail.com>
2021-02-19 10:14:34 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "HELLO WORLD!!! is changed";
+		return "Changed for 2nd time some webhook concept";
 	}
 	
 }
